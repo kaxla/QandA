4f443aa4085e8b222fb20c5c8125326f1d9b6073
kaxla <kaylas.email.address@gmail.com>
2014-01-28 19:39:47 -0800

    real first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QandA::Application.config.secret_key_base = 'a116b2d49c4c34ab64a166bfcbe4b87ac6267e236d17b22f6c2f18e9cff55edbc370643dcac48298ac53bbbf0b3313958dbe018ebff028fb8a0acdc72d269ef6'
